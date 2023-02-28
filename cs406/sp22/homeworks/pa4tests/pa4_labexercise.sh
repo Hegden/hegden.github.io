5f304c1bcbf2f9f5a81b5a64df80d363d7814883
nikhilh <nikhilh@iitdh.ac.in>
2023-02-28 11:47:41 +0530

    adding labexercise

---
+++ cs406/sp22/homeworks/pa4tests/pa4_labexercise.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm -rf inputs outputs
+unzip inputs.zip
+g++ -w tinyNew.C -o tiny
+echo "[*] make compiler"
+make compiler
+if [ $? -ne 0 ]
+then
+	echo "Test failed. Error in building compiler. (-5)"
+else
+	#echo "[*] ./runme test_combination.micro user_op.out"
+	bash runme inputs/labexercise.micro user_op.out
+	#echo "[*] ./tiny user_op.out"
+	str=`./tiny user_op.out`
+	echo $str | grep -q "hello"  
+	if [ $? -ne 0 ]
+	then  
+		echo "Test failed. Incorrect output / output format for test_combination.out (-5)"
+	else
+		echo "Test passed."
+	fi
+fi
